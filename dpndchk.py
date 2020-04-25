82a56821bac55cce0666f108aa987214b74b8217
Trodskovich <Trodskovich@gmail.com>
2020-04-25 08:36:12 +0530

    pending error catch

--- dpndchk.py
+++ dpndchk.py
@@ -1,1 +1,33 @@
+# -----------------------------------------------------------------------------------------------------------------------------
+# NvDrIN v1=0.1
+# dpndchk
+# Dependency Checker
+# To check for Necessary Dependencies before  Install Commands-Install Nvidia Driver (The Hard Way).
+# Part of NvDrIn (Nvidia Driver Installer) Project
+# Authour: Trodskovich
+# requires python3-newt and newt preinstalled
+# Run at your own Risk
+# -----------------------------------------------------------------------------------------------------------------------------
 
+import subprocess
+#from snack import SnackScreen, GridForm, ButtonBar, Textbox, CheckboxTree, snackArgs
+
+# class for the colors to use in printing messages
+class blkColors:
+    Header = "\033[95m"
+    Blue = "\033[94m"
+    Green = "\033[92m"
+    Warning = "\033[93m"
+    Error = "\033[91m"
+    EndC = "\033[0m"
+    Bold = "\033[1m"
+    Undeline = "\033[4m"
+    
+# Clear the screen
+#subprocess.call("clear", shell=True)
+
+
+
+print(blkColors.Blue + "\ncheck for Necessary Dependencies...\n" + blkColors.EndC)
+subprocess.call(["sudo zypper in -t pattern devel_C_C++ devel_kernel"], shell=True)
+print(blkColors.Blue + "\nDependency Check Complete.\n" + blkColors.EndC)
--- postinstall.py
+++ postinstall.py
@@ -1,1 +1,31 @@
+# -----------------------------------------------------------------------------------------------------------------------------
+# NvDrIN v1=0.1
+# PostInstall
+# Check Latest Driver
+# To run Post Install Commands-Install Nvidia Driver (The Hard Way).
+# Part of NvDrIn (Nvidia Driver Installer) Project
+# Authour: Trodskovich
+# requires python3-newt and newt preinstalled
+# Run at your own Risk
+# -----------------------------------------------------------------------------------------------------------------------------
 
+import subprocess
+#from snack import SnackScreen, GridForm, ButtonBar, Textbox, CheckboxTree, snackArgs
+
+# class for the colors to use in printing messages
+class blkColors:
+    Header = "\033[95m"
+    Blue = "\033[94m"
+    Green = "\033[92m"
+    Warning = "\033[93m"
+    Error = "\033[91m"
+    EndC = "\033[0m"
+    Bold = "\033[1m"
+    Undeline = "\033[4m"
+    
+# Clear the screen
+subprocess.call("clear", shell=True)
+
+print(blkColors.Blue + "\nUpdating Initramfs, Grub and rebooting...\n" + blkColors.EndC)
+subprocess.call(["sudo mkinitrd ; grub2-mkconfig ; reboot"], shell=True)
+print(blkColors.Blue + "\nPost Install Script Complete.\n" + blkColors.EndC)
